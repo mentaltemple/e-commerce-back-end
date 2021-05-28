5c17476828d36334eb44785935bc92e137c63811
Joshua Abeyta <mentaltemple@gmail.com>
2021-05-27 21:26:53 -0600

    fixed tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,7 +7,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all tags
   try {
-    const tagData = await Category.findAll({
+    const tagData = await Tag.findAll({
       include: [{ model: Product }],
     });
     res.status(200).json(tagData);
@@ -21,12 +21,12 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Category.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
     if (!tagData) {
-      res.status(404).json({ message: "No category found with this id!" });
+      res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
   } catch (err) {
@@ -39,7 +39,7 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   // create a new tag
   try {
-    const tagData = await Category.create(req.body);
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -50,15 +50,15 @@ router.post("/", (req, res) => {
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const categoryData = await Category.update(req.body, {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(tagData);
 
-    if (!categoryData) {
-      res.status(404).json({ message: "No category found with this id!" });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
   } catch (err) {
@@ -70,14 +70,14 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Category.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: res.params.id,
       },
     });
 
     if (!tagData) {
-      res.status(404).json({ message: "No category found with this id!" });
+      res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
   } catch (err) {
